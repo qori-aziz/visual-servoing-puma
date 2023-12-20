a05f891b6efa0cee81696950214e4ff768ed1fda
Qori Aziz <muhamadqori@gmail.com>
2023-12-20 13:29:04 +0700

    commit depth reg

--- depth_regression.py
+++ depth_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 
 # x = np.array([
 #     [206.2868217, 162.4392765],
@@ -32,6 +33,7 @@ model = LinearRegression()
 model.fit(x,y)
 
 r_sq = model.score(x, y)
+print("----------------Linear Regression----------------")
 print(f"coefficient of determination: {r_sq}")
 print(f"intercept: {model.intercept_}")
 print(f"slope: {model.coef_}")
@@ -44,10 +46,54 @@ model = LinearRegression().fit(x_, y)
 
 x_1 = PolynomialFeatures(degree=2, include_bias=False).fit_transform(x[0].reshape(1,-1))
 r_sq = model.score(x_, y)
+
+print("----------------Polynomial Regression----------------")
 print(f"coefficient of determination: {r_sq}")
 
 print(f"intercept: {model.intercept_}")
 
 print(f"coefficients: {model.coef_}")
-y_pred = model.predict(x_1)
+y_pred = model.predict(x_)
 print(f"predicted response:\n{y_pred}")
+
+## Calculate mean square error
+Y_true = np.array([
+    [40],
+    [50],
+    [70],
+    [100],
+    [150],
+    [200],
+    [220],
+    [250],
+    [270],
+])
+
+Y_pred_linear = np.array([
+    [4],
+    [77.78],
+    [57.294],
+    [143.37],
+    [165.23],
+    [199.39],
+    [206.22],
+    [214.41],
+    [222.61],
+])
+
+Y_pred_quadratic = np.array([
+    [49.631],
+    [28.606],
+    [24.35],
+    [94.414],
+    [134.02],
+    [213.6],
+    [232.11],
+    [255.45],
+    [280.04],
+])
+mse_linear = mean_squared_error(Y_true,Y_pred_linear)
+mse_quadratic = mean_squared_error(Y_true,Y_pred_quadratic)
+
+print(f"MSE Linear : {mse_linear}")
+print(f"MSE Quadratic : {mse_quadratic}")
